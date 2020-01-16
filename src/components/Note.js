4ca48155ce6d2f2e8df973374378f45eb37c84ac
Hiter Harris <hiterharris@comcast.net>
2020-01-16 11:23:36 -0500

    content editable

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,10 +2,9 @@ import React from 'react';
 import '../App.css';
 
 function Note(props) {
-  console.log(props.selectedNote);
   return (
     <div className='Note'>
-        <textarea className='text-area' type="text" defaultValue={props.selectedNote.text} value={props.selectedNote} />
+        <div contenteditable="true">{props.selectedNote}</div>
     </div>
   );
 }
