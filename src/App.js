f2d74b77cd51a5b9b9f95d2b8603959dfe3bb8ca
Hiter Harris <hiterharris@comcast.net>
2020-01-20 17:57:56 -0500

    set up data server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { notesData } from './data/notesData';
 import List from './components/List';
 import Note from './components/Note';
 import Nav from './components/Nav';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
-    const [notes, setNotes] = useState(notesData);
-    const [selectedNote, setSelectedNote] = useState(notesData[1].text)
+    const [notes, setNotes] = useState([]);
+    const [selectedNote, setSelectedNote] = useState(notesData[1].text);
+
+    useEffect(() => {
+        axios.get('http://localhost:3333/notes')
+            .then(response => {
+                console.log(response.data);
+                setNotes(response.data);
+            })
+    }, [0]);
+
+
 
     const addNote = title => {
         const newNote = {
