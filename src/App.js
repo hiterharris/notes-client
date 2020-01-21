e95ceb8eff888f8400742cc19910028391ace859
Hiter Harris <hiterharris@comcast.net>
2020-01-20 21:52:02 -0500

    added new express server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { notesData } from './data/notesData';
+// import { notesData } from './data/notesData';
 import List from './components/List';
 import Note from './components/Note';
 import Nav from './components/Nav';
@@ -8,17 +8,16 @@ import './App.css';
 
 const App = () => {
     const [notes, setNotes] = useState([]);
-    const [selectedNote, setSelectedNote] = useState(notesData[1].text);
+    const [selectedNote, setSelectedNote] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3333/notes')
+        axios.get('http://localhost:3001/notes')
             .then(response => {
                 console.log(response.data);
                 setNotes(response.data);
+                setSelectedNote(response.data[1].text)
             })
     }, [0]);
-
-
 
     const addNote = title => {
         const newNote = {
